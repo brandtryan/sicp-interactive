a1676aa5ecfa3963e4ea9b291a7c58489fb614e5
Brandt Ryan <No public email provided>
2025-05-26 21:01:01 -0400

    1.1.8_p25.js

---
+++ 1.1.8_p25.js
@@ -0,0 +1,27 @@
+function sqrt(x) {
+    return sqrt_iter(1, x);
+}
+
+function sqrt_iter(guess, x) {
+    return is_good_enough(guess, x)
+        ? guess
+        : sqrt_iter(improve(guess, x), x);
+}
+
+function is_good_enough(guess, x) {
+    return math_abs(square(guess) -x) < 0.001;
+}
+
+function improve(guess, x) {
+    return average(guess, x / guess);
+}
+
+function square(x) {
+    return x * x;
+}
+
+function average(x, y) {
+    return (x + y) / 2;
+}
+
+sqrt(4);
